fbb9ca50ed1bb4ffc66d9b65a47075c623def00b
lennysoft90 <ogu4leo@gmail.com>
2020-01-24 04:14:12 +0100

    implememted firebase utils included ability to store authenticated user

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-const HatsPage = ()=> (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 
 class App extends React.Component {
   constructor() {
@@ -26,9 +22,29 @@ class App extends React.Component {
     
 
     componentDidMount() {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({ currentUser: user });
-        console.log(user);
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if (userAuth) {
+            const userRef = await createUserProfileDocument(userAuth);
+
+            userRef.onSnapshot(snapShot => {
+              this.setState({
+                currentUser: {
+                  id : snapShot.id,
+                  ...snapShot.data()
+                }
+              }
+              )
+              console.log(this.state);
+            });
+            
+        }
+        else{
+          this.setState({currentUser: userAuth});
+
+          
+        }
+        
+
       });
     
     } 
